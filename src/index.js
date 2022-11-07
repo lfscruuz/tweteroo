982d1d4e2ff1a4b5b21a99e5d7c1b3e164a393d2
luis <lfscruz2001@gmail.com>
2022-11-07 14:03:38 -0300

    lista de tweets atuaalizando com 10 tweets

--- src/index.js
+++ src/index.js
@@ -7,32 +7,88 @@ app.use(express.json());
 
 const usuarios = [
     {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+        username: "cachorro",
+        avatar: "http://s2.glbimg.com/cYa3pKAKIPidjKyGPuAd8T4Hd1I=/e.glbimg.com/og/ed/f/original/2017/08/21/dog-2570398_960_720.jpg"
     },
     {
-        username: "patrick",
-        avatar: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Patrick-Star.SpongeBob-SquarePants.webp"
+        username: "gato",
+        avatar: "https://jpimg.com.br/uploads/2020/10/cat-528386_1280.jpg"
+    },
+    {
+        username: "tucano",
+        avatar: "https://www.petz.com.br/blog/wp-content/uploads/2022/06/animais-selvagens-3.jpg"
+    },
+    {
+        username: "leao",
+        avatar: "https://www.petz.com.br/blog/wp-content/uploads/2022/07/animais-da-africa3.jpg"
+    },
+    {
+        username: "tatu",
+        avatar: "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/01-amored-animals-nationalgeographic_1159498.jpg?w=1600&h=900"
+    },
+    {
+        username: "arara",
+        avatar: "https://leiturinha.com.br/blog/wp-content/uploads/2022/06/iStock-1314796448-1.jpg"
+    },
+    {
+        username: "llama",
+        avatar: "https://t1.ea.ltmcdn.com/pt/posts/4/1/1/os_animais_mais_fofos_do_mundo_23114_600_square.jpg"
+    },
+    {
+        username: "onca",
+        avatar: "https://reallylearnportuguese.com/wp-content/uploads/2022/04/onca-770x460.jpg"
+    },
+    {
+        username: "golfinho",
+        avatar: "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/o-golfinho-um-exemplo-animal-pertencente-ao-grupo-dos-mamiferos-5bd334f3235f1.jpg"
+    },
+    {
+        username: "cobrafofa",
+        avatar: "https://pbs.twimg.com/media/ERrYO5QWAAE88D9.jpg"
     }
 ];
 
 const tweets = [
     {
-        username: "bobesponja",
-      tweet: "eu amo o hub"
+        username: "cobrafofa",
+        tweet: "boop. boop the snoot"
     },
     {
-        username: "patrick",
-      tweet: "va dao cu"
+        username: "onca",
+        tweet: "mamifero mais pica que tem"
     },
     {
-        username: "lulamolusc0",
-      tweet: "va dao cu"
+        username: "golfinho",
+        tweet: "mamifero mais fdp que tem"
     },
     {
-        username: "sirigueijo",
-      tweet: "va dao cu"
-    }
+        username: "cachorro",
+        tweet: "auau"
+    },
+    {
+        username: "gato",
+        tweet: "miau"
+    },
+    {
+        username: "tucano",
+        tweet: "barulho de tucano"
+    },
+    {
+        username: "leao",
+        tweet: "preguicinha"
+    },
+    {
+        username: "tatu",
+        tweet: "em casa agora"
+    },
+    {
+        username: "arara",
+        tweet: "ahaha"
+    },
+    {
+        username: "llama",
+        tweet: "*cuspo na sua cara"
+    },
 ];
 
 app.get('/sign-up', (req, res) => {
@@ -41,7 +97,6 @@ app.get('/sign-up', (req, res) => {
 
 app.post('/sign-up', (req, res) => {
     const {username, avatar} = req.body;
-    console.log(req.body);
     const novoUsuario = {
         username: username,
         avatar: avatar
@@ -59,8 +114,7 @@ app.get('/tweets', (req, res) =>{
             avatar: dadosUsuario.avatar,
             tweet: tweets[i].tweet
         })
-        console.log(mostrarTweet)
-        dadosUsuario !== undefined ? novosTweets.push(mostrarTweet) : ''
+        novosTweets.push(mostrarTweet)
     }
     res.send(novosTweets);
 })
@@ -71,7 +125,10 @@ app.post('/tweets', (req, res) =>{
         username: username,
         tweet: tweet
     };
-    tweets.push(novoTweet);
+    if (tweets.length = 10){
+        tweets.pop(tweets[10])
+    }
+    tweets.unshift(novoTweet);
     res.send('OK');
 })
 app.listen(5000);
